25fd1aaa70ed4e227b949ffe548380dcd7285174
Kian Ahrabian <kian@cafebazaar.ir>
2017-02-16 14:48:05 +0330

    1. Change output format 2. Minor fix

--- get_threshold.py
+++ get_threshold.py
@@ -9,16 +9,16 @@ PATH = os.path.dirname(__file__)
 
 if __name__ == '__main__':
     with open(PATH + 'genuine.txt', 'r') as f:
-        px = np.sort(np.array(f.read().split('\n')[0]).astype(np.float))
+        px = np.sort(np.array(f.read().split('\n')[:-1]).astype(np.float))
 
     with open(PATH + 'forged.txt', 'r') as f:
-        nx = np.sort(np.array(f.read().split('\n')[0]).astype(np.float))
+        nx = np.sort(np.array(f.read().split('\n')[:-1]).astype(np.float))
 
     threshold = 0
     min_dist = np.matmul(px, px.T) + np.matmul(nx, nx.T)
     for i in itertools.chain(px, nx):
         filtered_px = px[px < i]
-        filtered_nx = nx[nx < i]
+        filtered_nx = nx[nx > i]
         dist = np.linalg.norm(filtered_px - np.full(filtered_px.shape, i)) + \
             np.linalg.norm(filtered_nx - np.full(filtered_nx.shape, i))
 
@@ -26,6 +26,6 @@ if __name__ == '__main__':
             min_dist = dist
             threshold = i
 
-    with open(PATH + 'threshold.txt', 'a') as f:
-        f.write(str(threshold) + '\n')
-        f.write(str(min_dist) + '\n')
+    with open(PATH + 'threshold.txt', 'w') as f:
+        f.write('Threshold: {t}\n'.format(t=str(threshold)))
+        f.write('Min Distance: {d}\n'.format(d=str(min_dist)))
