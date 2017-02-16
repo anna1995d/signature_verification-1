c261e8b8968cb9bc33d2f1200f153819c6558161
Kian Ahrabian <kian@cafebazaar.ir>
2017-02-16 14:18:08 +0330

    Threshold generator

---
+++ get_threshold.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import itertools
+import numpy as np
+
+PATH = os.path.dirname(__file__)
+
+
+if __name__ == '__main__':
+    with open(PATH + 'genuine.txt', 'a') as f:
+        px = np.sort(map(int, f.read().split('\n')))
+
+    with open(PATH + 'forged.txt', 'a') as f:
+        nx = np.sort(map(int, f.read().split('\n')))
+
+    threshold = 0
+    min_dist = np.matmul(px, px.T) + np.matmul(nx, nx.T)
+    for i in itertools.chain(px, nx):
+        filtered_px = px[px < i]
+        filtered_nx = nx[nx < i]
+        dist = np.linalg.norm(filtered_px - np.full(filtered_px.shape, i)) + \
+            np.linalg.norm(filtered_nx - np.full(filtered_nx.shape, i))
+
+        if dist < min_dist:
+            min_dist = dist
+            threshold = i
+
+    with open(PATH + 'threshold.txt', 'a') as f:
+        f.write(str(threshold) + '\n')
+        f.write(str(min_dist) + '\n')
