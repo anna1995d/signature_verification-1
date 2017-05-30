818196b3ccc8903f63492890220b34d7365b5222
Kian Ahrabian <kian@cafebazaar.ir>
2017-05-30 09:15:07 +0430

    Train only one autoencoder

--- main.py
+++ main.py
@@ -12,18 +12,24 @@ def process_models():
     prepare_output_directory()
     lsvcs_dir = prepare_lsvc_output_directory(CONFIG.out_dir)
     prepare_lsvc_evaluations_csv(lsvcs_dir)
+    xtmp, ytmp = list(), list()
     for usr_num in range(1, CONFIG.usr_cnt + 1):
-        x, y, tr_gen_x, tr_frg_x, gen_x, frg_x = get_autoencoder_train_data(DATA, usr_num - 1)
-        e = load_encoder(x, y, usr_num)
+        x, y = get_autoencoder_train_data(usr_num - 1)[:2]
+        xtmp.append(x)
+        ytmp.append(y)
+
+    import numpy as np
+    x = np.concatenate(xtmp, axis=0)
+    y = np.concatenate(ytmp, axis=0)
+    e = load_encoder(x, y, 1)
+
+    for usr_num in range(1, CONFIG.usr_cnt + 1):
+        tr_gen_x, tr_frg_x, gen_x, frg_x = get_autoencoder_train_data(usr_num - 1)[2:]
 
         tr_enc_gen = get_encoded_data(e, tr_gen_x)
         tr_enc_frg = get_encoded_data(e, tr_frg_x)
         enc_gen = get_encoded_data(e, gen_x)
         enc_frg = get_encoded_data(e, frg_x)
-
-        print(enc_gen)
-        print(enc_frg)
-
         save_encoded_representations(usr_num, tr_enc_gen, enc_gen, enc_frg)
 
         import numpy as np
