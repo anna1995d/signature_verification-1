704e2a1c11205e41c1496e0032da006915fc90c4
Kian Ahrabian <kian@cafebazaar.ir>
2017-06-03 00:27:29 +0430

    Refactor code according to single autoencoder format

--- main.py
+++ main.py
@@ -2,44 +2,36 @@
 
 from utils.config import CONFIG
 from utils.data import DATA
-from utils.evaluation.lsvc import prepare_lsvc_output_directory, evaluate_lsvc, prepare_lsvc_evaluations_csv, \
+from utils.evaluation.lsvc import evaluate_lsvc, prepare_lsvc_evaluations_csv, get_lsvc_train_data, train_lsvc, \
     save_lsvc_evaluation, save_lsvc_avg_evaluation
-from utils.io import save_encoded_representations, prepare_output_directory
-from utils.rnn import get_autoencoder_train_data, load_encoder, get_encoded_data
+from utils.io import prepare_output_directory
+from utils.rnn import get_autoencoder_train_data, get_autoencoder_evaluation_data, load_encoder, get_encoded_data
 
 
 def process_models():
     prepare_output_directory()
-    lsvcs_dir = prepare_lsvc_output_directory(CONFIG.out_dir)
-    prepare_lsvc_evaluations_csv(lsvcs_dir)
-    xtmp, ytmp = list(), list()
-    for usr_num in range(1, CONFIG.usr_cnt + 1):
-        x, y = get_autoencoder_train_data(usr_num - 1)[:2]
-        xtmp.append(x)
-        ytmp.append(y)
 
-    import numpy as np
-    x = np.concatenate(xtmp, axis=0)
-    y = np.concatenate(ytmp, axis=0)
-    e = load_encoder(x, y, 1)
+    prepare_lsvc_evaluations_csv()
+
+    x, y = get_autoencoder_train_data()
+    e = load_encoder(x, y)
 
     for usr_num in range(1, CONFIG.usr_cnt + 1):
-        tr_gen_x, tr_frg_x, gen_x, frg_x = get_autoencoder_train_data(usr_num - 1)[2:]
+        tr_gen_x, tr_frg_x, gen_x, frg_x = get_autoencoder_evaluation_data(usr_num - 1)
 
-        tr_enc_gen = get_encoded_data(e, tr_gen_x)
-        tr_enc_frg = get_encoded_data(e, tr_frg_x)
         enc_gen = get_encoded_data(e, gen_x)
         enc_frg = get_encoded_data(e, frg_x)
-        save_encoded_representations(usr_num, tr_enc_gen, enc_gen, enc_frg)
 
         import numpy as np
-        evl = evaluate_lsvc(usr_num, np.nan_to_num(enc_gen), np.nan_to_num(enc_frg), lsvcs_dir)
+        x, y = get_lsvc_train_data(np.nan_to_num(enc_gen), np.nan_to_num(enc_frg))
+        c = train_lsvc(x, y)
+        evl = evaluate_lsvc(c, x, y, usr_num)
         evl.update({
             CONFIG.lsvc_csv_fns[1]: np.mean(list(map(len, DATA.gen[usr_num - 1]))),
             CONFIG.lsvc_csv_fns[2]: np.mean(list(map(len, DATA.frg[usr_num - 1])))
         })
-        save_lsvc_evaluation(evl, lsvcs_dir)
-    save_lsvc_avg_evaluation(lsvcs_dir)
+        save_lsvc_evaluation(evl)
+    save_lsvc_avg_evaluation()
 
 
 if __name__ == '__main__':
--- utils/evaluation/lsvc.py
+++ utils/evaluation/lsvc.py
@@ -8,23 +8,18 @@ from seq2seq.models import LinearSVC
 from utils.config import CONFIG
 
 
-def prepare_lsvc_output_directory(out_dir):
-    lsvcs_dir = os.path.join(out_dir, 'linear_svcs')
-    if not os.path.exists(lsvcs_dir):
-        os.mkdir(lsvcs_dir)
-
-    return lsvcs_dir
-
-
 def get_lsvc_train_data(enc_gen, enc_frg):
     return np.concatenate((enc_gen, enc_frg)), \
            np.concatenate((np.ones_like(enc_gen[:, 0]), np.zeros_like(enc_frg[:, 0])))
 
 
-def train_lsvc(x, y, usr_num, lsvcs_dir):
+def train_lsvc(x, y):
     c = LinearSVC()
     c.fit(x, y)
-    c.save(path=os.path.join(lsvcs_dir, CONFIG.mdl_save_temp.format(usr_num=usr_num)))
+    return c
+
+
+def evaluate_lsvc(c, x, y, usr_num):
     cr = list(map(float, classification_report(y_true=y, y_pred=c.predict(x)).split('\n')[-2].split()[3:6]))
     return {
         CONFIG.lsvc_csv_fns[0]: usr_num,
@@ -34,25 +29,20 @@ def train_lsvc(x, y, usr_num, lsvcs_dir):
     }
 
 
-def evaluate_lsvc(usr_num, enc_gen, enc_frg, lsvcs_dir):
-    x, y = get_lsvc_train_data(enc_gen, enc_frg)
-    return train_lsvc(x, y, usr_num, lsvcs_dir)
-
-
-def prepare_lsvc_evaluations_csv(lsvcs_dir):
-    with open(os.path.join(lsvcs_dir, 'evaluations.csv'), 'w') as f:
+def prepare_lsvc_evaluations_csv():
+    with open(os.path.join(CONFIG.out_dir, 'lsvc_evaluations.csv'), 'w') as f:
         w = csv.DictWriter(f, fieldnames=CONFIG.lsvc_csv_fns)
         w.writeheader()
 
 
-def save_lsvc_evaluation(evl, lsvcs_dir):
-    with open(os.path.join(lsvcs_dir, 'evaluations.csv'), 'a') as f:
+def save_lsvc_evaluation(evl):
+    with open(os.path.join(CONFIG.out_dir, 'lsvc_evaluations.csv'), 'a') as f:
         w = csv.DictWriter(f, fieldnames=CONFIG.lsvc_csv_fns)
         w.writerow(evl)
 
 
-def save_lsvc_avg_evaluation(lsvcs_dir):
-    with open(os.path.join(lsvcs_dir, 'evaluations.csv'), 'r') as f:
+def save_lsvc_avg_evaluation():
+    with open(os.path.join(CONFIG.out_dir, 'lsvc_evaluations.csv'), 'r') as f:
         avg = {
             CONFIG.lsvc_csv_fns[0]: 'AVG'
         }
@@ -63,6 +53,6 @@ def save_lsvc_avg_evaluation(lsvcs_dir):
             ) for i in range(1, len(CONFIG.lsvc_csv_fns))
         })
 
-    with open(os.path.join(lsvcs_dir, 'evaluations.csv'), 'a') as f:
+    with open(os.path.join(CONFIG.out_dir, 'lsvc_evaluations.csv'), 'a') as f:
         w = csv.DictWriter(f, fieldnames=CONFIG.lsvc_csv_fns)
         w.writerow(avg)
