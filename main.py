b701e7bc47bb59879cccf60e480cea2c0c2315cc
Kian Ahrabian <kian@cafebazaar.ir>
2017-06-14 16:44:26 +0430

    New method to prepare data for SVC

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from utils.evaluation.svc import evaluate_svc, prepare_svc_evaluations_csv, get_svc_train_data, train_svc, \
-    save_svc_evaluation, save_svc_avg_evaluation, get_svc_evaluation_data
+    save_svc_evaluation, get_svc_evaluation_data
 from utils.io import prepare_output_directory
 from utils.rnn import get_autoencoder_train_data, load_encoder
 
@@ -16,10 +16,9 @@ def process_models():
     c = train_svc(x, y)
 
     prepare_svc_evaluations_csv()
-    for usr_num, (x, y) in enumerate(zip(*get_svc_evaluation_data(e)), 1):
-        evl = evaluate_svc(c, x, y, usr_num)
-        save_svc_evaluation(evl)
-    save_svc_avg_evaluation()
+    x, y = get_svc_evaluation_data(e)
+    evl = evaluate_svc(c, x, y, 'AVG')
+    save_svc_evaluation(evl)
 
 
 if __name__ == '__main__':
--- utils/evaluation/svc.py
+++ utils/evaluation/svc.py
@@ -31,32 +31,37 @@ def _get_svc_data(e, usr_num_gen):
             np.mean(np.min(ref_dists, axis=1)), np.min(ref_mdists), np.mean(np.max(ref_dists, axis=1))
         ], ndmin=2)
 
-        gen_x = np.nan_to_num(np.concatenate([
+        ref_mean_vec = np.mean(np.concatenate([
+            np.min(ref_dists, axis=1, keepdims=True),
+            np.mean(ref_dists, axis=1, keepdims=True),
+            np.max(ref_dists, axis=1, keepdims=True)
+        ], axis=1), axis=0, keepdims=True)
+
+        gen_x = np.nan_to_num((np.concatenate([
             np.min(gen_dists, axis=1, keepdims=True),
             np.mean(gen_dists[:, np.argmin(ref_mdists)].reshape((-1, 1)), axis=1, keepdims=True),
             np.max(gen_dists, axis=1, keepdims=True)
-        ], axis=1) / feat_vec)
-        frg_x = np.nan_to_num(np.concatenate([
+        ], axis=1) - ref_mean_vec) / feat_vec)
+        frg_x = np.nan_to_num((np.concatenate([
             np.min(frg_dists, axis=1, keepdims=True),
             np.mean(frg_dists[:, np.argmin(ref_mdists)].reshape((-1, 1)), axis=1, keepdims=True),
             np.max(frg_dists, axis=1, keepdims=True)
-        ], axis=1) / feat_vec)
+        ], axis=1) - ref_mean_vec) / feat_vec)
         x.append(np.concatenate([gen_x, frg_x]))
 
         gen_y = np.ones_like(gen_x[:, 0])
         frg_y = np.zeros_like(frg_x[:, 0])
         y.append(np.concatenate([gen_y, frg_y]))
 
-    return x, y
+    return np.concatenate(x, axis=0), np.concatenate(y, axis=0)
 
 
 def get_svc_train_data(e):
-    x, y = list(map(lambda vec: np.concatenate(vec, axis=0), _get_svc_data(e, range(CONFIG.svc_tr_usr_cnt))))
-    return x, y
+    return _get_svc_data(e, range(CONFIG.svc_tr_usr_cnt))
 
 
 def train_svc(x, y):
-    c = NuSVC(nu=0.79, gamma=0.0001, verbose=CONFIG.verbose)
+    c = NuSVC(nu=0.79, gamma=0.000027, verbose=CONFIG.verbose)
     c.fit(x, y)
     return c
 
@@ -69,8 +74,8 @@ def evaluate_svc(c, x, y, usr_num):
     cr = list(map(float, classification_report(y_true=y, y_pred=c.predict(x)).split('\n')[-2].split()[3:6]))
     return {
         CONFIG.svc_csv_fns[0]: usr_num,
-        CONFIG.svc_csv_fns[1]: np.mean(list(map(len, DATA.gen[usr_num - 1]))),
-        CONFIG.svc_csv_fns[2]: np.mean(list(map(len, DATA.frg[usr_num - 1]))),
+        # CONFIG.svc_csv_fns[1]: np.mean(list(map(len, DATA.gen[usr_num - 1]))),
+        # CONFIG.svc_csv_fns[2]: np.mean(list(map(len, DATA.frg[usr_num - 1]))),
         CONFIG.svc_csv_fns[3]: cr[0],
         CONFIG.svc_csv_fns[4]: cr[1],
         CONFIG.svc_csv_fns[5]: cr[2]
