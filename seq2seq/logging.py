cc560d290975d5118bd9603a59e780b558117b5e
Kian Ahrabian <kian@cafebazaar.ir>
2017-07-17 19:28:03 +0430

    Fix batch logger

--- seq2seq/logging.py
+++ seq2seq/logging.py
@@ -6,7 +6,9 @@ logger = logging.getLogger(__name__)
 
 elogger = LambdaCallback(
     on_epoch_end=lambda epoch, logs: logger.info(
-        'EPOCH #{epoch}: loss: {loss}, acc: {acc}'.format(epoch=epoch, loss=logs['loss'], acc=logs['acc'])
+        'EPOCH #{epoch}: loss: {loss}, acc: {acc}, val_loss: {val_loss}, val_acc: {val_acc}'.format(
+            epoch=epoch, loss=logs['loss'], acc=logs['acc'], val_loss=logs['val_loss'], val_acc=logs['val_acc']
+        )
     )
 )
 
