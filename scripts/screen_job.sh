c7d7dbb7a17dc4bb6dcdbcf648882e2ac7d98725
Kian Ahrabian <kian@cafebazaar.ir>
2017-07-18 16:57:37 +0430

    Set keras backend to tensorflow inside the script

--- scripts/screen_job.sh
+++ scripts/screen_job.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source activate sigv
-TF_CPP_MIN_LOG_LEVEL=3 screen -dmS sigv -L python main.py
+TF_CPP_MIN_LOG_LEVEL=3 KERAS_BACKEND=tensorflow screen -dmS sigv -L python main.py
 source deactivate sigv
