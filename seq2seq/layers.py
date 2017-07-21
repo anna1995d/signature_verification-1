3def06923a1fa560c79c6d22a38ecaa1c7b2932c
Kian Ahrabian <kian@cafebazaar.ir>
2017-07-21 13:38:55 +0430

    New attention layer added

--- seq2seq/layers.py
+++ seq2seq/layers.py
@@ -73,3 +73,60 @@ class AttentionWithContext(Layer):
 
     def compute_output_shape(self, input_shape):
         return input_shape[0], input_shape[-1]
+
+
+class Attention(Layer):
+    def __init__(self,
+                 kernel_regularizer=None, bias_regularizer=None,
+                 kernel_constraint=None, bias_constraint=None,
+                 use_bias=True, **kwargs):
+        self.kernel = None
+        self.bias = None
+
+        self.supports_masking = True
+        self.kernel_initializer = initializers.get('glorot_uniform')
+
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+
+        self.use_bias = use_bias
+
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.kernel = self.add_weight(
+            name='kernel',
+            shape=(input_shape[-1], input_shape[-1],),
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint
+        )
+        if self.use_bias:
+            self.bias = self.add_weight(
+                name='bias',
+                shape=(input_shape[-1],),
+                initializer='zero',
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint
+            )
+        else:
+            self.bias = None
+
+        super(Attention, self).build(input_shape)
+
+    def compute_mask(self, inputs, mask=None):
+        return None
+
+    def call(self, inputs, mask=None):
+        eij = K.tanh(K.dot(inputs, self.kernel) + (self.bias if self.use_bias else 0))
+        ai = K.exp(eij) * K.expand_dims(K.cast(mask, K.floatx()) if mask is not None else 1)
+        a = ai / K.cast(K.sum(ai, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+        return K.sum(inputs * a, axis=1)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], input_shape[-1]
