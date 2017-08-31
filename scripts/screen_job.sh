ef31ddfe7a50a3a101d340af7197ea500defecb1
Kian Ahrabian <kian@cafebazaar.ir>
2017-08-31 10:30:12 +0430

    Ignore warnings

--- scripts/screen_job.sh
+++ scripts/screen_job.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 source activate sigv
-KERAS_BACKEND=theano screen -dmS sigv -L python main.py
+KERAS_BACKEND=theano screen -dmS sigv -L python -W ignore main.py
 source deactivate sigv
