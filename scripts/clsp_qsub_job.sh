25c95b7f49d9ade43cc643175ea7c78cd60d453b
Kian Ahrabian <kian@cafebazaar.ir>
2017-09-24 01:06:20 +0330

    Try to fix theano backend array

--- scripts/clsp_qsub_job.sh
+++ scripts/clsp_qsub_job.sh
@@ -16,5 +16,5 @@
 #$ -pe smp 8
 
 source activate sigv
-CUDA_VISIBLE_DEVICES=`free-gpu` python -W ignore main.py
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=`free-gpu` python -W ignore main.py
 source deactivate sigv
