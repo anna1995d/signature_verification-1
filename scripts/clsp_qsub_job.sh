84930eadb358572e36d2dc0f6335a27e03d7fd53
Kian Ahrabian <kian@cafebazaar.ir>
2017-10-12 00:21:17 +0330

    Update resources, fix cpu

--- scripts/clsp_qsub_job.sh
+++ scripts/clsp_qsub_job.sh
@@ -13,7 +13,7 @@
 #$ -l 'arch=*64*'
 #$ -l mem_free=32G,ram_free=32G
 #$ -l "hostname=b*|c*"
-#$ -pe smp 16
+#$ -pe smp 8
 
 source activate sigv
 CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=`free-gpu` python -W ignore main.py
