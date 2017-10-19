fbf91f92ba0f852f4b89e14e35b5af1962bf2508
Kian Ahrabian <kian@cafebazaar.ir>
2017-10-19 11:47:43 +0330

    Fix padding

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -23,8 +23,7 @@ def get_siamese_data(encoder, fold):
             mean = np.mean(encoded_genuine[:CONFIG.ref_smp_cnt], axis=0)
             std = np.std(encoded_genuine[:CONFIG.ref_smp_cnt], axis=0, ddof=1) + np.finfo(np.float32).eps
 
-            encoded_genuine = (encoded_genuine - mean) / std
-            encoded_forgery = (encoded_forgery - mean) / std
+            encoded_genuine, encoded_forgery = (encoded_genuine - mean) / std, (encoded_forgery - mean) / std
 
             x.extend(map(lambda z: np.array(z, ndmin=3), itertools.product(
                 encoded_genuine[:CONFIG.ref_smp_cnt], encoded_genuine[:CONFIG.ref_smp_cnt]
@@ -64,8 +63,7 @@ def get_siamese_data(encoder, fold):
             mean = np.mean(encoded_genuine, axis=0)
             std = np.std(encoded_genuine, ddof=1) + np.finfo(np.float32).eps
 
-            encoded_genuine = (encoded_genuine - mean) / std
-            encoded_forgery = (encoded_forgery - mean) / std
+            encoded_genuine, encoded_forgery = (encoded_genuine - mean) / std, (encoded_forgery - mean) / std
 
             x.extend(map(lambda z: np.array(z, ndmin=3), itertools.product(encoded_genuine, encoded_genuine)))
             y.extend(np.ones((len(encoded_genuine) * len(encoded_genuine), 1)))
